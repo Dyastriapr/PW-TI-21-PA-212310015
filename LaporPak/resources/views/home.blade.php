0f774855556436cb2b01784ce40018527a3597d3
Dyas Tri <dyastriap12@gmail.com>
2023-04-06 21:53:28 +0700

    LaporPak

---
+++ LaporPak/resources/views/home.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaporPak</title>
+
+    <link href="style.css" rel="stylesheet">
+    
+
+</head>
+<body>
+    <div class="hero">
+        <nav>
+            <h2> <div class="logo">Lapor<span style="color: orange">Pak</span></div></h2>
+            <ul>
+                <li><a href="#">Tentang LaporPak</a></li>
+                <li><a href="#">Statistik</a></li>
+             
+            </ul>
+        </nav>
+
+        <div class="content">
+           
+            <h1>Layanan Pengaduan <span style="color : orange"> Dan Aspirasi</span> </h1>
+            <h4> Sampaikan laporan anda langsung kepada kantor polisi terdekat</h4>
+            
+        </div>
+        <div class="box">
+            <div class="box2"><h3>Sampaikan Laporan Anda</h3></div>
+            <div class="pengaduan">
+                <h5 style="color: white">Pilih Jenis Pengaduan: </h5>
+            
+            
+            </div>
+            <div class="box3">
+
+            <button class="button" type="button"> PENGADUAN </button>
+            <button class="button" type="button"> ASPIRASI </button>
+            </div>
+
+                <div class="box4">
+                <form action="" method="get" id="form1">
+                <input type="text" id="judul" name="judul" style=" width: 605px ; height: 40px" placeholder="    Masukan Judul Laporan Anda"><br>
+                <textarea id="textinput" name="textinput" rows="4" cols="85" placeholder="    Masukan Isi Laporan Anda"></textarea>
+                <input type="text" id="lokasi" name="lokasi" style=" width: 605px ; height: 40px"  placeholder="    Masukan Lokasi Kejadian"><br>
+                <input type="text" id="polisi" name="polisi" style=" width: 605px ; height: 40px"  placeholder="    Daftar Polisi Terdekat"><br>
+             
+            
+
+                <button class ="button" type="submit" form="form1" value="Submit">LAPOR</button>
+
+
+
+
+                </div>
+
+            </div>
+            
+
+    </div>
+
+    <div class="about">
+        <div class="main"> 
+            <div class="about-text">
+                <h2>JUMLAH <span style="color : orange"> LAPORAN </span></h2>
+                <h5> 899,989</h5>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>Copyright 2023.Dyas Tri Apriliansyah <p>
+    </footer>
+   
+
+
+    
+</body>
+</html>
